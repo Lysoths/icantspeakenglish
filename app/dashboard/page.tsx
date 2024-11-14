aaaf62310f80e064644a5cb0f392193b7bb19fb0
Lysoth <ogzhner5@gmail.com>
2024-11-14 20:08:42 +0300

    .

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import WordForm from "@/components/WebForm";
 
 export default function Dashboard() {
   return (
-    <div className="min-h-screen p-4">
+    <div className="min-h-[%90] p-4">
       <div className="flex w-full items-center justify-between">
         <LogoutButton />
         <h1 className="text-3xl font-bold mb-4">Kelime Ekleme</h1>
--- components/WebForm.tsx
+++ components/WebForm.tsx
@@ -121,86 +121,87 @@ export default function WordForm() {
   };
 
   return (
-    <div className="p-4 max-w-lg mx-auto">
-      <h2 className="text-2xl font-bold mb-4">
-        {editingWordId ? "Kelimeyi Güncelle" : "Yeni Kelime Ekle"}
-      </h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label className="block text-gray-700 font-semibold mb-2">
-            İngilizce Kelime
-          </label>
-          <input
-            type="text"
-            value={englishWord}
-            onChange={(e) => setEnglishWord(e.target.value)}
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="İngilizce kelime girin"
-          />
-        </div>
+    <div className="p-4 justify-center items-center mx-auto flex md:flex-row flex-col w-full gap-3 border border-gray-600 rounded-xl mt-6 ">
+      <div className="bg-[--arkaPlan] p-4 rounded-sm w-[400px] h-[550px]">
+        <h2 className="text-2xl font-bold mb-4">
+          {editingWordId ? "Kelimeyi Güncelle" : "Yeni Kelime Ekle"}
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-semibold mb-2">
+              İngilizce Kelime
+            </label>
+            <input
+              type="text"
+              value={englishWord}
+              onChange={(e) => setEnglishWord(e.target.value)}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="İngilizce kelime girin"
+            />
+          </div>
 
-        <div className="mb-4">
-          <label className="block text-gray-700 font-semibold mb-2">
-            Türkçe Anlamlar
-          </label>
-          {turkishTranslations.map((translation, index) => (
-            <div key={index} className="flex items-center mb-2">
-              <input
-                type="text"
-                value={translation}
-                onChange={(e) => handleTranslationChange(index, e.target.value)}
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder={`Türkçe anlam ${index + 1}`}
-              />
-            </div>
-          ))}
+          <div className="mb-4">
+            <label className="block text-gray-700 font-semibold mb-2">
+              Türkçe Anlamlar
+            </label>
+            {turkishTranslations.map((translation, index) => (
+              <div key={index} className="flex items-center mb-2">
+                <input
+                  type="text"
+                  value={translation}
+                  onChange={(e) =>
+                    handleTranslationChange(index, e.target.value)
+                  }
+                  className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder={`Türkçe anlam ${index + 1}`}
+                />
+              </div>
+            ))}
+            <button
+              type="button"
+              onClick={handleAddTranslation}
+              className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-lg font-semibold hover:bg-blue-600 transition-colors"
+            >
+              Yeni Anlam Ekle
+            </button>
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 font-semibold mb-2">
+              Resim URL
+            </label>
+            <input
+              type="text"
+              value={imgUrl}
+              onChange={(e) => setImgUrl(e.target.value)}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="Resim URL'si girin"
+            />
+          </div>
+
           <button
-            type="button"
-            onClick={handleAddTranslation}
-            className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-lg font-semibold hover:bg-blue-600 transition-colors"
+            type="submit"
+            className="w-full bg-green-500 text-white py-2 rounded-lg font-semibold hover:bg-green-600 transition-colors"
           >
-            Yeni Anlam Ekle
+            {editingWordId ? "Güncelle" : "Kaydet"}
           </button>
-        </div>
 
-        <div className="mb-4">
-          <label className="block text-gray-700 font-semibold mb-2">
-            Resim URL
-          </label>
-          <input
-            type="text"
-            value={imgUrl}
-            onChange={(e) => setImgUrl(e.target.value)}
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Resim URL'si girin"
-          />
-        </div>
-
-        <button
-          type="submit"
-          className="w-full bg-green-500 text-white py-2 rounded-lg font-semibold hover:bg-green-600 transition-colors"
-        >
-          {editingWordId ? "Güncelle" : "Kaydet"}
-        </button>
-
-        {message && (
-          <p className="mt-4 text-center text-lg font-semibold text-green-600">
-            {message}
-          </p>
-        )}
-      </form>
+          {message && (
+            <p className="mt-4 text-center text-lg font-semibold text-green-600">
+              {message}
+            </p>
+          )}
+        </form>
+      </div>
 
       <div>
-        <h2 className="text-xl font-bold mt-8 mb-4">
-          Kelime Sayısı : {words.length}
-        </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 overflow-y-auto h-[560px] ">
           {words.map((word) => (
             <div
               key={word.id}
-              className="bg-purple-100 p-4 rounded-lg shadow-md border border-purple-200 flex flex-col items-center justify-between"
+              className="bg-purple-100 p-4 rounded-lg shadow-md border border-purple-200 flex flex-col items-center justify-between "
             >
-              <div className="flex items-center flex-col">
+              <div className="flex items-center flex-col ">
                 <h3 className="text-lg font-semibold text-purple-700 mb-1">
                   {word.english}
                 </h3>
@@ -221,7 +222,10 @@ export default function WordForm() {
               )}
               <div className="flex justify-end mt-2 space-x-2">
                 <button
-                  onClick={() => handleEdit(word)}
+                  onClick={() => {
+                    window.scrollTo({ top: 0, behavior: "smooth" });
+                    handleEdit(word);
+                  }}
                   className="bg-blue-500 text-white px-3 py-1 rounded-lg font-semibold hover:bg-blue-600 transition-colors"
                 >
                   Düzenle
